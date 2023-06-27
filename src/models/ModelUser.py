4cc190f9a455ffcce7e99e8761865a9a01f29e56
Tomas Wardoloff <tomaswardoloff@gmail.com>
2023-06-27 00:38:53 -0300

    user session managment

--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -10,10 +10,19 @@ class ModelUser:
         """.format(user.Email)
         match_user = get_data(database, query)
         if match_user != []:
-            user = User(
+            return User(
                 match_user[0][0],
                 match_user[0][1],
                 User.check_password(match_user[0][2], user.Password),
             )
-            return user
+        return None
+
+    @classmethod
+    def get_user_by_id(cls, database, id):
+        query = """
+            SELECT UserId, Email FROM users WHERE UserId = '{}'
+        """.format(id)
+        match_user = get_data(database, query)
+        if match_user != []:
+            return User(match_user[0][0], match_user[0][1], None)
         return None
--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -1,11 +1,16 @@
 from werkzeug.security import check_password_hash
+from flask_login import UserMixin
 
-class User:
+
+class User(UserMixin):
     def __init__(self, id, email, password) -> None:
         self.UserId = id
         self.Email = email
         self.Password = password
-    
+
     @classmethod
-    def check_password(self, hashed_password, password):
+    def check_password(cls, hashed_password, password):
         return check_password_hash(hashed_password, password)
+
+    def get_id(self):
+        return self.UserId
