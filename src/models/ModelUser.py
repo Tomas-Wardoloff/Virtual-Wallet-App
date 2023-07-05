41190d120983bd3c775dd676717c5f25263cb0d5
Tomas Wardoloff <tomaswardoloff@gmail.com>
2023-07-04 21:14:51 -0300

    manage exceptions

--- src/models/ModelUser.py
+++ src/models/ModelUser.py
@@ -9,7 +9,7 @@ class ModelUser:
             SELECT UserId, Email, Password FROM users WHERE Email = '{}'
         """.format(user.Email)
         match_user = get_data(database, query)
-        if match_user != []:
+        if match_user != ():
             return User(
                 match_user[0][0],
                 match_user[0][1],
@@ -23,6 +23,6 @@ class ModelUser:
             SELECT UserId, Email FROM users WHERE UserId = '{}'
         """.format(id)
         match_user = get_data(database, query)
-        if match_user != []:
+        if match_user != ():
             return User(match_user[0][0], match_user[0][1], None)
         return None
--- src/models/database.py
+++ src/models/database.py
@@ -1,7 +1,14 @@
+from MySQLdb import ProgrammingError, OperationalError 
 def get_data(database, query: str) -> list:
-    cursor = database.connection.cursor()
     try:
+        cursor = database.connection.cursor()
         cursor.execute(query)
         return cursor.fetchall()
+    except ProgrammingError as ex:
+        raise ("Error de sintaxis en la consulta SQL: {}".format(ex))
+    except OperationalError as ex:
+        raise Exception("Error de conexión a la base de datos: {}".format(ex))
     except Exception as ex:
-        raise Exception(ex)
+        raise Exception("Error inesperado: {}".format(ex))
+    finally:
+        database.connection.close()
