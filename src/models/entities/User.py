29a9603c2c0fd1cd9042201876915de38d93f436
Tomas Wardoloff <tomaswardoloff@gmail.com>
2023-07-05 12:34:02 -0300

    change user class

--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -4,13 +4,10 @@ from flask_login import UserMixin
 
 class User(UserMixin):
     def __init__(self, id, email, password) -> None:
-        self.UserId = id
-        self.Email = email
-        self.Password = password
+        self.id = id
+        self.email = email
+        self.password = password
 
     @classmethod
     def check_password(cls, hashed_password, password):
         return check_password_hash(hashed_password, password)
-
-    def get_id(self):
-        return self.UserId
