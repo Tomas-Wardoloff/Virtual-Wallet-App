e2365807d6fcb1b48d111c2de36918c18d61f103
Tomas Wardoloff <tomaswardoloff@gmail.com>
2023-07-07 10:40:21 -0300

    create function hash passwords

--- src/models/entities/User.py
+++ src/models/entities/User.py
@@ -1,4 +1,4 @@
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
 
@@ -13,3 +13,7 @@ class User(UserMixin):
     @classmethod
     def check_password(cls, hashed_password, password):
         return check_password_hash(hashed_password, password)
+    
+    @classmethod
+    def hash_password(cls, password: str) -> str:
+        return generate_password_hash(password)
